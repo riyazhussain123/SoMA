f3c1b65ab4379f1a0de4b897b348469e2987a01e
Arjun Venkatraman <arjun@mojolab.org>
2017-10-25 15:02:19 +0530

    Working on framweork

--- python/lib/libsoma.py
+++ python/lib/libsoma.py
@@ -1,5 +1,15 @@
 '''
 Contains SoMA Core functions and classes
 '''
+from uuid import *
+import json
+from collections import namedtuple
 
-
+class SoMAPost():
+	def __init__(self,jsoncontent=None):
+		self.uuid=uuid4()
+		if jsoncontent==None:
+			self.content=None
+		else:
+			self.attributes=json.loads(jsoncontent,object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+			self.dictionary=json.loads(jsoncontent)
