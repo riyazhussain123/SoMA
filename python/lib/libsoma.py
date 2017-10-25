817e9217c7e0b115f04cd78e8e802a8c95f6d08c
Arjun Venkatraman <arjun@mojolab.org>
2017-10-25 15:08:25 +0530

    Documenting

--- python/lib/libsoma.py
+++ python/lib/libsoma.py
@@ -5,11 +5,19 @@ from uuid import *
 import json
 from collections import namedtuple
 
+
+'''
+SoMA Post objects store a JSON object as a named tuple as well as a dictionary, so that attributes can be referenced both in the dot notation as well as by key/value lookups. 
+'''
+
 class SoMAPost():
 	def __init__(self,jsoncontent=None):
 		self.uuid=uuid4()
 		if jsoncontent==None:
 			self.content=None
 		else:
+			#Got this nice little object factory like implementation from https://stackoverflow.com/questions/6578986/how-to-convert-json-data-into-a-python-object for the dot notation support
 			self.attributes=json.loads(jsoncontent,object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+			#This just loads the same content into a dict as well
 			self.dictionary=json.loads(jsoncontent)
+		
