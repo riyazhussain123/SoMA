c5471eefad413ef3f12efb4f0cbfbc84c49e459b
Arjun Venkatraman <arjun@mojolab.org>
2017-11-04 16:00:34 +0530

    Added

--- python/scripts/selenium/fbbase.py
+++ python/scripts/selenium/fbbase.py
@@ -1,21 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import ConfigParser
-configfile="/opt/fbid1.conf"
-config=ConfigParser.ConfigParser()
-config.read(configfile)
-usr = config.get("User","username")
-pwd = config.get("User","password")
 
-driver = webdriver.Firefox()
-# or you can use Chrome(executable_path="/usr/bin/chromedriver")
-driver.get("http://www.facebook.org")
-assert "Facebook" in driver.title
-elem = driver.find_element_by_id("email")
-elem.send_keys(usr)
-elem = driver.find_element_by_id("pass")
-elem.send_keys(pwd)
-elem.send_keys(Keys.RETURN)
+def setup(configfile):
+	config=ConfigParser.ConfigParser()
+	config.read(configfile)
+	return config
+	
+def login_to_fb(configfile):
+	config=setup(configfile)
+	usr = config.get("User","username")
+	pwd = config.get("User","password")
+	firefox_profile = webdriver.FirefoxProfile()
+	firefox_profile.set_preference("browser.privatebrowsing.autostart", True)
+	driver = webdriver.Firefox(firefox_profile=firefox_profile)
+	# or you can use Chrome(executable_path="/usr/bin/chromedriver")
+	driver.get("http://www.facebook.org")
+	assert "Facebook" in driver.title
+	elem = driver.find_element_by_id("email")
+	elem.send_keys(usr)
+	elem = driver.find_element_by_id("pass")
+	elem.send_keys(pwd)
+	elem.send_keys(Keys.RETURN)
+	return driver
 
 #elem = driver.find_element_by_css_selector(".input.textInput")
 #elem.send_keys("Posted using Python's Selenium WebDriver bindings!")
@@ -25,3 +32,4 @@ elem.send_keys(Keys.RETURN)
 
 
 #driver.close()
+
---
+++ python/scripts/selenium/fbdriver.py
@@ -0,0 +1,8 @@
+import sys
+from fbbase import *
+
+if __name__=="__main__":
+	configfile="/opt/fbid1.conf"
+
+	driver=login_to_fb(configfile)
+
